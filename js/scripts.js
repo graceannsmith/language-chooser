97ce73cb080dd8d49ad6cbb3dd36f68b37d21ec9
Graceann Smith <graceannsmith@gmail.com>
2023-11-05 14:22:23 -0800

    add window.onload

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@
 
 
 let result = "";
+
+
+
+
 function findLanguage(answer) {
   if (answer === "Sudoku") {
     result = "You should learn Python!";
@@ -15,16 +19,30 @@ function findLanguage(answer) {
 }
 
 
-
 //User
+window.onload = function () {
 
-function handleLanguage(event) {
-  event.preventDefault();
-  // the code to get and process form values will go here!
-}
+  const choiceOne = document.querySelector('label[for="sudoku"]');
+  const choiceTwo = document.querySelector('label[for="crossword"]');
+  const choiceThree = document.querySelector('label[for="wordsearch"]');
+  // Select the form element
+  const form = document.querySelector('#formOne');
 
-window.addEventListener("load", function () {
-  const form = document.getElementById("calculator");
-  form.addEventListener("submit", handleCalculation);
-});
+  // Select the input element within the form
+  const inputElement = form.querySelector('#myInput');
 
+  // Select the button to trigger the action
+  const submitButton = document.querySelector('#submitButton');
+
+  // Add an event listener to the button to get the value
+  submitButton.addEventListener('click', function () {
+    // Get the value of the input element
+    const value = inputElement.value;
+
+    // Do something with the value
+    console.log('The value of the input element is: ' + value);
+  });
+
+
+
+};
