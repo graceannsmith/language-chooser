8279cb7213ff61610ff879fe0be9d088e68f1ace
Graceann Smith <graceannsmith@gmail.com>
2023-11-05 19:41:35 -0800

    add event handlers

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,34 @@
 //Business
-
-
 let result = "";
 
-
-
-
 function findLanguage(answer) {
-  if (answer === "Sudoku") {
-    result = "You should learn Python!";
-  } else if (answer === "Crossword") {
-    "You should learn HTML!";
-  } else {
+  if (answer === "SUDOKU") {
     result = "You should learn javaScript!";
+  } else if (answer === "CROSSWORD") {
+    result = "You should learn C#!";
+  } else {
+    result = "You should learn python]!";
   }
-
   return result;
 }
 
 
+
 //User
 window.onload = function () {
+  let form = document.querySelector("form");
 
-  const choiceOne = document.querySelector('label[for="sudoku"]');
-  const choiceTwo = document.querySelector('label[for="crossword"]');
-  const choiceThree = document.querySelector('label[for="wordsearch"]');
-  // Select the form element
-  const form = document.querySelector('#formOne');
-
-  // Select the input element within the form
-  const inputElement = form.querySelector('#myInput');
-
-  // Select the button to trigger the action
-  const submitButton = document.querySelector('#submitButton');
-
-  // Add an event listener to the button to get the value
-  submitButton.addEventListener('click', function () {
-    // Get the value of the input element
-    const value = inputElement.value;
-
-    // Do something with the value
-    console.log('The value of the input element is: ' + value);
-  });
+  //let selected = document.querySelector("input:checked").value;
 
 
+  form.onsubmit = function (event) {
+    let selected = document.querySelector("input:checked").value;
 
+    findLanguage(selected);
+    document.querySelector("h2").innerText = result;
+    event.preventDefault();
+  }
 };
+
+
+
